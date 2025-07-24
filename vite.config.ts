72db9efd323d720b624f1c032775b170d22f4393
Wilzard95 <william.mendoza8995@gmail.com>
2025-07-24 17:18:47 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/APK-Posgrados-Controldeactividades/',
   plugins: [react()],
+  build: {
+    outDir: 'docs', // <--- Cambia esto de 'dist' a 'docs'
+  },
+  base: '/APK-Posgrados-Controldeactividades/', // importante para rutas correctas
 });
